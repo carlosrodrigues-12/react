6fe8048831ee9c33428fbdd2a27b48b17e7533ea
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2020-07-30 17:50:26 -0300

    Configuracao dos campos de categoria

--- megaflix/src/pages/cadastro/Categoria/index.js
+++ megaflix/src/pages/cadastro/Categoria/index.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
+  
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria {nomeDaCategoria}</h1>
 
       <form>
+
         <label>
           Nome da Categoria:
           <input
             type="text"
+            value={nomeDaCategoria}
+            onChange={() => {
+              console.log('[nomeDaCategoria]', nomeDaCategoria);
+            }}
           />
         </label>
 
         <button>
           Cadastrar
         </button>
+
       </form>
-      
+
       <Link to="/">
         Ir para home
       </Link>
