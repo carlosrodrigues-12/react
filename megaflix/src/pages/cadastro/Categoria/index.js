51eab550451a1d9f30e775341af301476e29975e
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2020-07-31 16:53:20 -0300

    Configuracao da pagina de categorias, metodos adicionados

--- megaflix/src/pages/cadastro/Categoria/index.js
+++ megaflix/src/pages/cadastro/Categoria/index.js
@@ -3,31 +3,88 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [categorias, setCateorias] = useState(['Teste']);
 
-  const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
+  const valoresIniciais = {
+    nome: 'Carlos Rodrigues',
+    descricao: 'Descrição',
+    cor: '#BCA123',
+  }
+
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor){
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
+  }
   
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria {nomeDaCategoria}</h1>
+      
+      <h1>Cadastro de Categoria {values.nome}</h1>
 
-      <form>
+      <form onSubmit={(infosDoEvento) => {
+        infosDoEvento.preventDefault();
+        setCateorias([
+          ...categorias,
+          {values}
+        ]);
+      }}>
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-            value={nomeDaCategoria}
-            onChange={() => {
-              console.log('[nomeDaCategoria]', nomeDaCategoria);
-            }}
-          />
-        </label>
+        <div>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+              value={values.nome}
+              onChange={(infosDoEvento) => {
+                setValue('nome', infosDoEvento.target.value);
+              }}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              onChange={(infosDoEvento) => {
+                setValue('descricao', infosDoEvento.target.value);
+              }}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor}
+              onChange={(infosDoEvento) => {
+                setValues('cor', infosDoEvento.target.value);
+              }}
+            />
+          </label>
+        </div>
 
         <button>
           Cadastrar
         </button>
 
       </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return(
+            <li key={`${categoria} ${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
 
       <Link to="/">
         Ir para home
